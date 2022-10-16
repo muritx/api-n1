d6294d91ffa55cf4ea78ee9d6850a1e9d9ac28e7
Murilo Teixeira <murilosantosx@hotmail.com>
2022-10-16 00:23:18 -0300

    alteracao

--- app.py
+++ app.py
@@ -7,46 +7,46 @@ app = Flask(__name__)
 def main():
     return "Periféricos"
 
-# #@app.route("/aparelhos", methods=['GET'])
-# def aparelhos_get():
-#     return aparelhos
+#@app.route("/aparelhos", methods=['GET'])
+def aparelhos_get():
+    return aparelhos
 
-# @app.route("/aparelhos", methods=['POST'])
-# def aparelhos_creat():
-#     novo_aparelho = request.json
-#     aparelhos.append(novo_aparelho)
-#     return aparelhos
+@app.route("/aparelhos", methods=['POST'])
+def aparelhos_creat():
+    novo_aparelho = request.json
+    aparelhos.append(novo_aparelho)
+    return aparelhos
 
-# @app.route("/aparelhos", methods=['DELETE'])
-# def aparelhos_delet():
-#     delet_aparelho = request.json
-#     if delet_aparelho in aparelhos:
-#         aparelhos.remove(delet_aparelho)
-#         return aparelhos
-#     else:
-#         return "<h1>Aparelho não encontrado!</h1>"
+@app.route("/aparelhos", methods=['DELETE'])
+def aparelhos_delet():
+    delet_aparelho = request.json
+    if delet_aparelho in aparelhos:
+        aparelhos.remove(delet_aparelho)
+        return aparelhos
+    else:
+        return "<h1>Aparelho não encontrado!</h1>"
     
-# @app.route("/aparelhos", methods=['PUT'])
-# def aparelho_put():
-#     put_aparelho = request.json
-#     id = put_aparelho.get("id")
-#     nome = put_aparelho.get("nome")
+@app.route("/aparelhos", methods=['PUT'])
+def aparelho_put():
+    put_aparelho = request.json
+    id = put_aparelho.get("id")
+    nome = put_aparelho.get("nome")
     
-#     aparelhos[id]["nome"] = nome
+    aparelhos[id]["nome"] = nome
         
-#     return aparelhos 
+    return aparelhos 
  
-# @app.route("/aparelhos", methods=['PATCH'])
-# def aparelho_patch():
-#     patch_aparelho = request.json
-#     id = patch_aparelho.get("id")
-#     nome = patch_aparelho.get("nome")
-#     marca = patch_aparelho.get("marca")
+@app.route("/aparelhos", methods=['PATCH'])
+def aparelho_patch():
+    patch_aparelho = request.json
+    id = patch_aparelho.get("id")
+    nome = patch_aparelho.get("nome")
+    marca = patch_aparelho.get("marca")
     
-#     aparelhos[id]["nome"] = nome
-#     aparelhos[id]["marca"] = marca
+    aparelhos[id]["nome"] = nome
+    aparelhos[id]["marca"] = marca
         
-#     return aparelhos  
+    return aparelhos  
 
 if __name__ == "__main__":
     main()
