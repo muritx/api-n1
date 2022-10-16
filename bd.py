0162392ec5c91acc21007f7be31c7813f605499b
Murilo Teixeira <murilosantosx@hotmail.com>
2022-10-16 00:19:50 -0300

    alteracao

--- bd.py
+++ bd.py
@@ -1,17 +1,17 @@
 aparelhos = [
-        {
-            "id": 0,
-            "marca": "hyperx",
-            "nome": "Temp v2"
-        },
-        {
-            "id": 1,
-            "marca": "logitech",
-            "nome": "G922"
-        },
-        {
-            "id": 2,
-            "marca": "Razer",
-            "nome": "Basilisk"
-        }
-]
+    {
+      "id": 0,
+      "marca": "hyperx",
+      "nome": "Temp v2"
+    },
+    {
+      "id": 1,
+      "marca": "logitech",
+      "nome": "G922"
+    },
+    {
+      "id": 2,
+      "marca": "Razer",
+      "nome": "Basilisk"
+    }
+  ]
