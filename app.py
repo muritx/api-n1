3d33552da42d15554cb8816820ba3c503a62b856
Murilo Teixeira <murilosantosx@hotmail.com>
2022-10-15 23:31:49 -0300

    alteracoes

--- app.py
+++ app.py
@@ -1,17 +1,11 @@
 from flask import Flask, request
 from bd import aparelhos
-from flask_cors import CORS
 
 app = Flask(__name__)
 
-cors = CORS(app, resource={r"/*":{"origins": "*"}})
-
-def main():
-    port = int(os.environ.get("PORT",5000))
-    app.run(host="0.0.0.0", port=port)
-
-if __name__ == "__main__":
-    main()
+@app.route("/")
+def index():
+    return "Periféricos"
 
 @app.route("/aparelhos", methods=['GET'])
 def aparelhos_get():
@@ -53,4 +47,4 @@ def aparelho_patch():
     aparelhos[id]["marca"] = marca
         
     return aparelhos  
-app.run()
+
