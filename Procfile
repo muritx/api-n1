66b1beb5092edeb68ac2b20e0c551b9280762ce0
Murilo Teixeira <murilosantosx@hotmail.com>
2022-10-15 21:40:46 -0300

    alterar nome

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn api_peri:app
+web: gunicorn app:app
--- app.py
+++ app.py
@@ -3,19 +3,19 @@ from bd import aparelhos
 
 
 
-api_peri = Flask(__name__)
+app = Flask(__name__)
 
-@api_peri.route("/aparelhos", methods=['GET'])
+@app.route("/aparelhos", methods=['GET'])
 def aparelhos_get():
     return aparelhos
 
-@api_peri.route("/aparelhos", methods=['POST'])
+@app.route("/aparelhos", methods=['POST'])
 def aparelhos_creat():
     novo_aparelho = request.json
     aparelhos.append(novo_aparelho)
     return aparelhos
 
-@api_peri.route("/aparelhos", methods=['DELETE'])
+@app.route("/aparelhos", methods=['DELETE'])
 def aparelhos_delet():
     delet_aparelho = request.json
     if delet_aparelho in aparelhos:
@@ -24,7 +24,7 @@ def aparelhos_delet():
     else:
         return "<h1>Aparelho não encontrado!</h1>"
     
-@api_peri.route("/aparelhos", methods=['PUT'])
+@app.route("/aparelhos", methods=['PUT'])
 def aparelho_put():
     put_aparelho = request.json
     id = put_aparelho.get("id")
@@ -34,7 +34,7 @@ def aparelho_put():
         
     return aparelhos 
  
-@api_peri.route("/aparelhos", methods=['PATCH'])
+@app.route("/aparelhos", methods=['PATCH'])
 def aparelho_patch():
     patch_aparelho = request.json
     id = patch_aparelho.get("id")
@@ -45,4 +45,4 @@ def aparelho_patch():
     aparelhos[id]["marca"] = marca
         
     return aparelhos  
-api_peri.run()
+app.run()
