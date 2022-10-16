5725058db8e823b39e3bcf31094c1592830502d2
Murilo Teixeira <murilosantosx@hotmail.com>
2022-10-15 22:26:44 -0300

    correcao

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 from flask import Flask, request
 from bd import aparelhos
-
-
+from flask_cors import CORS
 
 app = Flask(__name__)
+
+cors = CORS(app, resource={r"/*":{"origins": "*"}})
+
+def main():
+    port = int(os.environ.get("PORT",5000))
+    app.run(host="0.0.0.0", port=port)
+
+if __name__ == "__main__":
+    main()
 
 @app.route("/aparelhos", methods=['GET'])
 def aparelhos_get():
